365c82e1c24d6628d75d7b7348bc3b2efd45c1d6
Yuze Fu <i@xfox.me>
2024-06-15 00:57:10 +0900

    format: fix format

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ const server = createServer((req, res) => {
       res.writeHead(400, { "Content-Type": "text/plain" });
       res.end(
         "Origin header is required, and must be from vatprc.net: " +
-          req.headers.origin
+          req.headers.origin,
       );
       return;
     }
@@ -77,11 +77,11 @@ const server = createServer((req, res) => {
             "access-control-max-age",
             "access-control-request-method",
             "access-control-request-headers",
-          ].includes(key.toLowerCase())
+          ].includes(key.toLowerCase()),
         );
         if (
           resHeaders.find(
-            ([key]) => key.toLowerCase() === "access-control-allow-origin"
+            ([key]) => key.toLowerCase() === "access-control-allow-origin",
           ) === undefined
         ) {
           resHeaders.push(["Access-Control-Allow-Origin", req.headers.origin!]);
